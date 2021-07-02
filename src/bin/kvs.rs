2286d244c7a460beb10c6f1eee00dcf6c0b664fc
naokilh <821874555@qq.com>
2021-07-02 10:40:46 +0800

    123

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,43 +1,18 @@
-use clap::{App, AppSettings, Arg, SubCommand};
+#[macro_use]
+extern crate clap;
+use clap::App;
 use kvs::{KvStore, KvsError, Result};
 use std::env::current_dir;
 use std::process::exit;
 
 fn main() -> Result<()> {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::DisableHelpSubcommand)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("Set the value of a string key to a string")
-                .arg(Arg::with_name("KEY").help("A string key").required(true))
-                .arg(
-                    Arg::with_name("VALUE")
-                        .help("The string value of the key")
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("Get the string value of a given string key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("Remove a given key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
-        )
-        .get_matches();
+    let yaml = load_yaml!("kvs.yml");
+    let m = App::from_yaml(yaml).get_matches();
 
-    match matches.subcommand() {
+    match m.subcommand() {
         ("set", Some(matches)) => {
             let key = matches.value_of("KEY").unwrap();
             let value = matches.value_of("VALUE").unwrap();
-
             let mut store = KvStore::open(current_dir()?)?;
             store.set(key.to_string(), value.to_string())?;
         }
